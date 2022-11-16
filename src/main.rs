9b6370d7aab458141285927b38cc1a19dec7d02f
isaac <isaac@isaac.ac>
2022-11-16 12:01:28 +0000

    alias vec3 for color, use in print loop

--- src/main.rs
+++ src/main.rs
@@ -50,6 +50,8 @@ impl ops::Div for Vec3 {
     }
 }
 
+type Color = Vec3;
+
 fn main() {
     let image_w = 256;
     let image_h = 256;
@@ -59,13 +61,15 @@ fn main() {
     for j in (0..image_h).rev() {
         eprintln!("scanlines: {:3}/{}", j, image_h);
         for i in 0..image_w {
-            let r: f64 = (i as f64) / ((image_w - 1) as f64);
-            let g: f64 = (j as f64) / ((image_h - 1) as f64);
-            let b: f64 = 0.25;
+            let color: Color = Color {
+                x: (i as f64) / ((image_w - 1) as f64),
+                y: (j as f64) / ((image_h - 1) as f64),
+                z: 0.25,
+            };
 
-            let ir = (255.999 * r) as u32;
-            let ig = (255.999 * g) as u32;
-            let ib = (255.999 * b) as u32;
+            let ir = (255.999 * color.x) as u32;
+            let ig = (255.999 * color.y) as u32;
+            let ib = (255.999 * color.x) as u32;
 
             println!("{ir} {ig} {ib}")
         }
